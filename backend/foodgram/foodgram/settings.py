a5e8a07c97bd7af1ebcdb1bf8e2b28708eddfacf
Артемий Бутырин <artemii.butyrin@yandex.ru>
2022-10-10 17:16:23 +0300

    changed services.py

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -145,7 +145,7 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
-MEDIA_ROOT = '/var/html/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
 REST_FRAMEWORK = {
