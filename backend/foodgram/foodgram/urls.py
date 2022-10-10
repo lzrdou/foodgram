0141737cf68ca6857b36b113bcedc2aed476ddad
Артемий Бутырин <artemii.butyrin@yandex.ru>
2022-10-10 17:29:11 +0300

    changed services.py

--- backend/foodgram/foodgram/urls.py
+++ backend/foodgram/foodgram/urls.py
@@ -1,14 +1,14 @@
-from django.conf import settings
+# from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path, re_path
-from django.views.static import serve
+# from django.views.static import serve
 
 urlpatterns = [
     path("api/", include("api.urls", namespace="api")),
     path("admin/", admin.site.urls),
-    re_path(
-        r'^media/(?P<path>.*)$',
-        serve,
-        {'document_root': settings.MEDIA_ROOT, }
-    )
+    # re_path(
+    #     r'^media/(?P<path>.*)$',
+    #     serve,
+    #     {'document_root': settings.MEDIA_ROOT, }
+    # )
 ]
