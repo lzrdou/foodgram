e1ec31e0d23ef0c296d21eaee76c8b522775cfea
Артемий Бутырин <artemii.butyrin@yandex.ru>
2022-10-10 17:31:47 +0300

    changed services.py

--- backend/foodgram/foodgram/urls.py
+++ backend/foodgram/foodgram/urls.py
@@ -1,6 +1,7 @@
 # from django.conf import settings
 from django.contrib import admin
-from django.urls import include, path, re_path
+from django.urls import include, path
+# re_path
 # from django.views.static import serve
 
 urlpatterns = [
